d7d1cc4de1ffca73d7cbf82a0e39b93abb674499
ericgaspar <junk.eg@free.fr>
2021-09-16 11:51:07 +0200

    Fix

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -20,8 +20,12 @@ exit 1
 current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
 repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
-version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | .tag_name' | sort -V | tail -1)
+version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
 assets=($(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '[ .[] | select(.tag_name=="'$version'").assets[].browser_download_url ] | join(" ") | @sh' | tr -d "'"))
+
+if [[ ${version:0:1} == "v" || ${version:0:1} == "V" ]]; then
+     version=${version:1}
+fi
 
 # Setting up the environment variables
 echo "Current version: $current_version"
@@ -59,14 +63,14 @@ echo "Handling asset at $asset_url"
 # Here we base the source file name upon a unique keyword in the assets url (admin vs. update)
 # Leave $src empty to ignore the asset
 case $asset_url in
-  *"admin"*)
-    src="app"
+  *".zip"*)
+    src="arm64"
     ;;
-  *"update"*)
-    src="app-upgrade"
+  *".zip"*)
+    src="arm7"
     ;;
-  *)
-    src=""
+  *".zip"*)
+    src="x86-64"
     ;;
 esac
 
@@ -97,8 +101,8 @@ SOURCE_URL=$asset_url
 SOURCE_SUM=$checksum
 SOURCE_SUM_PRG=sha256sum
 SOURCE_FORMAT=$extension
-SOURCE_IN_SUBDIR=true
-SOURCE_FILENAME=
+SOURCE_IN_SUBDIR=false
+SOURCE_EXTRACT=true
 EOT
 echo "... conf/$src.src updated"
 
